71248f301761d57349145bfc5481a249179c357f
RickArmstrong1 <rick.r.armstrong@gmail.com>
2016-08-27 13:44:48 -0400

    added on to deaf grandma code

--- deafgrandma.rb
+++ deafgrandma.rb
@@ -2,17 +2,28 @@ puts "Hello young whipper-snapper! What have you been up to?"
 
 response = gets.chomp.to_s
 
-while response != "BYE"
+a = 0
 
-	if response == response.upcase
+while !(a == 2 && response == "BYE")
+
+	if response == "BYE"
+		puts "WHAT NOW? SAY AGAIN!"
+		response = gets.chomp.to_s
+		a += 1 
+	
+	elsif response == response.upcase
 		puts "NO NOT SINCE #{rand(1930..1980)}!".chomp
 		response = gets.chomp.to_s
+		a = 0
 	
 	elsif response != response.upcase
 		puts "HUH, SPEAK UP SONNY!".chomp
 		response = gets.chomp.to_s
-	end
-end	
+		a = 0
+		
+	end	
+end
+
 
 puts "BYE SONNY!"
 
